12804fde2829d060dc6e4c1937298b818141d6c5
osamos <122643939+Moyinoluwa360@users.noreply.github.com>
2024-12-05 01:16:33 +0100

    fixed an integer bug

--- question1-solution.py
+++ question1-solution.py
@@ -4,7 +4,7 @@
 import matplotlib.pyplot as plt
 
 items = ['Spinach', 'Sausage', 'Prawns', 'Pineapple', 'Mushroom']
-proportions = [0.27, 0.20, 0.12, 0.18, 0.15]
+proportions = [0.27, 0.20, 0.12, 0.27, 0.15]
 plt.barh(items, proportions, color='purple')
 plt.xlabel('Proportion of Total')
 plt.ylabel('Items')
